39361416db5b76045f6741cbcce3c919aaaae49a
Jonatan Cardoso <jonatancardosoc@gmail.com>
2023-11-17 03:04:49 -0300

    Search Adding search from project

--- dictionary-anjun/src/app/page.tsx
+++ dictionary-anjun/src/app/page.tsx
@@ -25,6 +25,17 @@ export default function Home() {
             />
           </div>
         </div>
+
+        <div className="relative w-full mx-5 my-10">
+          <textarea
+            className="p-1 h-14 text-black text-xl w-full rounded-lg resize-none bg-gray-light" required
+          />
+          <img
+            className='w-5 h-5 absolute inset-y-3 right-3'
+            src='iconSearch.svg'
+            alt='Icon Search'
+          />
+        </div>
       </div>
     </main>
   )
