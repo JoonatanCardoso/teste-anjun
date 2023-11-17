6f76f186fd5b07df5e2984ce08df4fc468105581
Jonatan Cardoso <jonatancardosoc@gmail.com>
2023-11-17 03:06:44 -0300

    Response Area Adding search response area

--- dictionary-anjun/src/app/page.tsx
+++ dictionary-anjun/src/app/page.tsx
@@ -36,6 +36,32 @@ export default function Home() {
             alt='Icon Search'
           />
         </div>
+
+        <div className="mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+          <p className='text-2xl font-bold text-black dark:text-white'>
+            Title
+          </p>
+
+          <div className="mt-10 flex items-center gap-x-4">
+            <h4 className='flex-none text-base font-bold text-black dark:text-white leading-6'>
+              Noun
+            </h4>
+            <div className="h-px flex-auto bg-gray" />
+          </div>
+
+          <div>
+            <p className='py-12 font-base font-medium text-gray'>Meanings</p>
+            <p className='flex'>
+              <img className='w-2' src="ellipse.svg" alt="Ellipse" />
+              <div className='pl-2 text-black dark:text-white'>(etc.) A set of keys used to operate a typewriter, computer etc.</div>
+            </p>
+          </div>
+
+          <div className='mt-10'>
+            <span className='text-base text-bold text-gray'>Synonyms</span>
+            <span className='ml-4	text-sm text-bold text-purble'>Electronic</span>
+          </div>
+        </div>
       </div>
     </main>
   )
